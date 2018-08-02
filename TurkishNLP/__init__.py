03c3cb13e2152ceb7632f5a60c3ee4842d005da3
ESAT MAHMUT BAYOL <36518895+brolin59@users.noreply.github.com>
2018-08-03 02:18:17 +0300

    Add files via upload

--- TurkishNLP/__init__.py
+++ TurkishNLP/__init__.py
@@ -1,1 +1,5 @@
-
+from TurkishNLP.TurkishGrammer import *
+from TurkishNLP.Tokenization import cumle_ayir
+from TurkishNLP.Morphological.MorphologicalLR import ClsEkBul as Ceb
+from TurkishNLP.Statistic import *
+from TurkishNLP.Asciidecoder.Asciidecoder import Deasciifier
---
+++ TurkishNLP/helpers.py
@@ -0,0 +1,1 @@
+from TurkishNLP.Auxiliary import *
