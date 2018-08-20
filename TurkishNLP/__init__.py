294315834b7f2ae61d500ea1b472d8c6dd3f64f8
ESAT MAHMUT BAYOL <36518895+brolin59@users.noreply.github.com>
2018-08-21 00:53:57 +0300

    Delete __init__.py

--- TurkishNLP/__init__.py
+++
@@ -1,5 +0,0 @@
-from TurkishNLP.TurkishGrammer import *
-from TurkishNLP.Tokenization import cumle_ayir
-from TurkishNLP.Morphological.MorphologicalLR import ClsEkBul as Ceb
-from TurkishNLP.Statistic import *
-from TurkishNLP.Asciidecoder.Asciidecoder import Deasciifier
