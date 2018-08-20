278407c1aa11605606d2245aa48dbeb8f3dbe013
ESAT MAHMUT BAYOL <36518895+brolin59@users.noreply.github.com>
2018-08-21 00:47:22 +0300

    Add files via upload

--- trnlp/__init__.py
+++ trnlp/__init__.py
@@ -1,1 +1,28 @@
+from trnlp.word_processing import *
+from trnlp.helpers import *
+from trnlp.Morphological.MorphologicalLR import ClsEkBul as Ceb
+from trnlp.Statistic.TextStatistic import Statistic as Stat
+from trnlp.Tokenization.SentenceTokenization import token, word_tokenize, tr_word_tokenize, sentence_token_wsign, \
+    sentence_token
+from trnlp.file_processing import *
 
+
+def deascii(str_grp: str):
+    from trnlp.Asciidecoder.Asciidecoder import Deasciifier
+    return Deasciifier(str_grp).convert_to_turkish()
+
+
+def find_suffix(word: str):
+    return Ceb(word).result
+
+
+def find_stems(word: str):
+    return Ceb(word).stems
+
+
+def unknown_words(str_grp: str):
+    return tr_word_tokenize(str_grp)[1]
+
+
+def view_statistic(str_grp: str):
+    return Stat(str_grp).text_statistic()
